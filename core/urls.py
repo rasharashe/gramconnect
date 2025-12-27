10052fcb10453a84952fc963cb19d11c3579234c
Rashad <rashadrashe@outlook.com>
2025-12-27 10:57:45 +0530

    clean signin page

--- core/urls.py
+++ core/urls.py
@@ -3,6 +3,6 @@ from django.http import HttpResponse
 from .views import Login, Home
 
 urlpatterns = [
-    path('signin/', Login.as_view(), name='signin'),
+    path('', Login.as_view(), name='signin'),
     path('home/', Home.as_view(), name='home'),
 ]
