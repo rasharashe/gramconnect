4dbffcb9468912755dac8906a5d4d54e65b3ff52
Rashad M <rashad.m@rhombes.com>
2026-01-03 15:40:22 +0530

    added dashboard

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from django.http import HttpResponse
-from .views import Login, HomeList, SquadList, bulk_upload, deployapp
+from .views import Login, HomeList, SquadList, DashboardView, bulk_upload, deployapp
 
 urlpatterns = [
     path('', Login.as_view(), name='signin'),
     path('home/', HomeList.as_view(), name='home'),
+    path('dashboard/', DashboardView.as_view(), name='dashboard'),
     path('upload/home/_bulk/', bulk_upload, name='home_bulk_upload'),
     path('deployapp/', deployapp, name='deploy_app'),
     path('squad/<str:sqid>/', SquadList.as_view(), name='squad_homes'),
